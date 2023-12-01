50fdb7a273e6dc1502258e7392a570fc7282577a
Omar Ahmed <>
2023-12-02 00:38:54 +0200

    - added spacing file for SizedBox. - added some colors & TextStyles. - added FontWeightHelper. - added general TextButton & general TextFormField. - Finished LoginScreen design.

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,16 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_project/core/widgets/app_text_form_field.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+import '../../../core/helpers/spacing.dart';
+import '../../../core/theming/styles.dart';
+import '../../../core/widgets/app_text_button.dart';
+import 'widgets/already_have_account_text.dart';
+import 'widgets/terms_and_conditions_text.dart';
+
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('LoginScreen'),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome Back',
+                  style: TextStyles.font24BlueBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style: TextStyles.font14GrayRegular,
+                ),
+                verticalSpace(36),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      const AppTextFormField(hintText: 'Email'),
+                      verticalSpace(18),
+                      AppTextFormField(
+                        hintText: 'Password',
+                        isObscureText: isObscureText,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isObscureText = !isObscureText;
+                            });
+                          },
+                          child: Icon(
+                            isObscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                      verticalSpace(24),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyles.font13BlueRegular,
+                        ),
+                      ),
+                      verticalSpace(40),
+                      AppTextButton(
+                        buttonText: "Login",
+                        textStyle: TextStyles.font16WhiteSemiBold,
+                        onPressed: () {},
+                      ),
+                      verticalSpace(16),
+                      const TermsAndConditionsText(),
+                      verticalSpace(60),
+                      const AlreadyHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        body: Center(
-          child: Text('LoginScreen is working'),
-        ));
+      ),
+    );
   }
 }
---
+++ lib/features/login/ui/widgets/already_have_account_text.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/theming/styles.dart';
+
+class AlreadyHaveAccountText extends StatelessWidget {
+  const AlreadyHaveAccountText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Already have an account?',
+            style: TextStyles.font13DarkBlueRegular,
+          ),
+          TextSpan(
+            text: ' Sign Up',
+            style: TextStyles.font13BlueSemiBold,
+          ),
+        ],
+      ),
+    );
+  }
+}
